555d45c535776f2ec129ababc14175babe769ce7
Kirill Lavrov <lkv@myttk.ru>
2021-08-19 00:01:00 +0700

    fix (script.js): Fixed support in Safari and background path

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const 
-  getKey = function(str, ...instructions ) {
+  getKey = function(str, instructions ) {
     return instructions.reduce(function(key, instruction) {
       if (!key) {
         key = str.match(instruction);
@@ -18,13 +18,9 @@ const
       let key;
 
       if (sortBy) {
-        key = getKey(document.querySelector(sortBy).textContent, 
-          /(?<=Книга )\d+/, /(?<=Глава )\d+/, /(?<=Приложение )[A-Z]+/
-          );
+        key = getKey(document.querySelector(sortBy).textContent, [/\d+/, /[A-Z]+/]);
       } else {
-        key = getKey(elem.textContent, 
-          /(?<=Книга )\d+/, /(?<=Глава )\d+/, /(?<=Приложение )[A-Z]+/
-          );
+        key = getKey(elem.textContent, [/\d+/, /[A-Z]+/]);
       }
       
       if (key) {
@@ -41,18 +37,18 @@ const
     // Удалить рекламу со страницы
     document.querySelector('.adv').remove();
     // Заменить картинку заднего фона на другую из папки image
-    document.body.style.backgroundImage = "url('/image/you-dont-know-js.jpg')";
+    document.body.style.backgroundImage = "url('image/you-dont-know-js.jpg')";
     // Восстановить порядок книг.
     sortElemList(books, 'a');
 
     books.forEach(function(book) {
       const bookTitle = book.querySelector('a');
 
-      if (getKey(bookTitle.textContent, /(Книга 3)/)) {
+      if (getKey(bookTitle.textContent, [/(Книга 3)/])) {
         // Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
         bookTitle.textContent = 'Книга 3. this и Прототипы Объектов';
       }
-      if (getKey(bookTitle.textContent, /(Книга 6)/)) {
+      if (getKey(bookTitle.textContent, [/(Книга 6)/])) {
         // В шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
         let newChapter = document.createElement('li');
         
@@ -60,7 +56,7 @@ const
         book.querySelector('ul').append(newChapter);
         sortElemList(book.querySelectorAll('li'));
       }
-      if (getKey(bookTitle.textContent, /(Книга 2)/) || getKey(bookTitle.textContent, /(Книга 5)/)) {
+      if (getKey(bookTitle.textContent, [/(Книга 2)/]) || getKey(bookTitle.textContent, [/(Книга 5)/])) {
         // Восстановить порядок глав во второй и пятой книге 
         sortElemList(book.querySelectorAll('li'));
       }  
